2524ca2c98f11570aadbab68735742a65542679f
jessicamwang <pandy10127@gmail.com>
2017-02-11 13:13:13 -0500

    postgres image actually working

--- application.py
+++ application.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "hello, world"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
