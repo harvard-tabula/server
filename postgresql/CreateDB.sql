45b4015af19481c2af181e63d208c07abc334c40
jessicamwang <pandy10127@gmail.com>
2017-02-13 23:19:53 -0500

    modified CreateDB.sql to reflect database structure discuessed 2.9.16, updated README to reflect how to access psql within Docker container

--- postgresql/CreateDB.sql
+++ postgresql/CreateDB.sql
@@ -1,4 +1,44 @@
-CREATE TABLE test_table (
-    student_id character varying(36) NOT NULL,
+CREATE TABLE users (
+    userId character varying(36) NOT NULL,
     email character varying(255)
+);
+
+CREATE TABLE profile (
+    userHash character varying(36) NOT NULL,
+    concentration character varying(255),
+    gender character varying(255),
+    ethnicity character varying(255),
+    codingYears int
+);
+
+CREATE TABLE courses (
+    courseId character varying(255) NOT NULL,
+    courseNumber int,
+    description character varying(255)
+);
+
+CREATE TABLE classHistory (
+    userHash character varying(36) NOT NULL,
+    courseId character varying(255) NOT NULL,
+    grade character varying(255),
+    semester character varying(255),
+    workload int,
+    enjoyment int,
+    learning int,
+    difficulty int
+);
+
+CREATE TABLE interests (
+	userHash character varying(36) NOT NULL,
+	interest int
+);
+
+CREATE TABLE languages (
+	userHash character varying(36) NOT NULL,
+	language int
+);
+
+CREATE TABLE milestones (
+	userHash character varying(36) NOT NULL,
+	milestone int
 );
