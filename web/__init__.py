b34fb35fc417debe447d6d13cdd9b218a0827f19
josephwandile <ec2-user@ip-172-31-72-14.ec2.internal>
2017-04-06 20:33:46 +0000

    cors works

--- web/__init__.py
+++ web/__init__.py
@@ -4,6 +4,6 @@ from . import config
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, allow_headers=['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'], origins='http://localhost:3000', supports_credentials=True)
+CORS(app, expose_headers=['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'], origins='http://localhost:3000', supports_credentials=True)
 app.config.from_object(config.DevelopmentConfig)
 db = SQLAlchemy(app)
